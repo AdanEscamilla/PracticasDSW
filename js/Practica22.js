e9444de393016c46edbb00786028639edd3cbe89
AdanEscamilla <adan.escamilla@uabc.edu.mx>
2023-10-03 18:09:10 -0700

    Carpeta con js de los projectos

---
+++ js/Practica22.js
@@ -0,0 +1,25 @@
+var numero1 = prompt("Ingrese el primero numero");
+var numero2 = prompt("Ingrese el segundo numero");
+var num1 = parseInt(numero1);
+var num2 = parseInt(numero2);
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multi = num1 * num2;
+var div = num1 / num2;
+
+alert("El resultado de la suma es: " + suma + "\nEl resultado de la resta es: " + resta + "\nEl resultado de la multiplicación es: " + multi + "\nEl resultado de la división es: " + div);
+
+/*var mensaje = "Bienvenido a nuestro sitio web";
+var texto_1 = " texto comillas dobles,";
+var texto_2 = ' texto comillas simples...';
+
+alert(mensaje + texto_1 + texto_2);
+
+var clienteRegistrado = false;
+var ivaIncluido = true;
+
+alert(clienteRegistrado);
+
+var array = ['Tijuana', 'Rosarito', 'Ensenada', 'Mexicali'];
+
+alert(array);*/
